ef9a406898f62be6fdab56974d13e1461518ae8d
Amanda <80905646+amanda-m-sa@users.noreply.github.com>
2025-10-30 07:39:46 -0300

    laravel_project

--- laravel-project/resources/views/posts.blade.php
+++ laravel-project/resources/views/posts.blade.php
@@ -3,7 +3,13 @@
     <h1>Posts</h1>
     <a href="{{ route('posts.create') }}">Novo post</a>
     @foreach ($posts as $post)
-     <h2>#{{ $post['id'] }} {{ $post['title'] }} - 
-        
-
+        <h2>#{{ $post['id'] }} {{ $post['title'] }} - 
+            <a href="{{ route('posts.edit', ['post' => $post['id']]) }}">editar</a>
+            <form action="{{ route('posts.destroy', ['post' => $post['id']]) }}" method="post">
+                @csrf 
+                @method('delete')
+                <input type="submit" value="excluir">
+            </form>
+        </h2>
+    @endforeach
 </div>
